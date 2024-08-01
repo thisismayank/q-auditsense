afda748e3d4d79c119a85252237720063ffe5015
Mayank Arya <mayank@getbasis.co>
2024-07-31 20:37:56 -0400

    Add styling to the form control labels

--- src/components/Questionnaire/facilityBasicInformation.component.js
+++ src/components/Questionnaire/facilityBasicInformation.component.js
@@ -9,6 +9,7 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
+  Card,
 } from "@mui/material";
 import { TextFieldQuestionnaire } from "./style";
 import moment from "moment";
@@ -19,6 +20,19 @@ const FacilityBasicInformation = ({
   handleChange,
   handleAddress,
 }) => {
+  const formControlLabelStyle = {
+    "& .MuiFormControlLabel-label": {
+      fontSize: "14px",
+      fontWeight: 500,
+      color: "#344054",
+    },
+    "& .MuiRadio-root": {
+      "& svg": {
+        width: "16px",
+        height: "16px",
+      },
+    },
+  };
   return (
     <>
       {/* <Typography sx={{ fontSize: 14, fontWeight: 600, float: "right" }}>
@@ -124,101 +138,165 @@ const FacilityBasicInformation = ({
             />
           </TextFieldQuestionnaire>
         </Grid> */}
-        <Grid item xs={12} sm={12}>
+        <Grid item xs={12} sm={12} sx={{ mb: 1 }}>
           <CountrySelector formData={formData} handleAddress={handleAddress} />
         </Grid>
 
-        <Grid item xs={12} sm={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Are Dormitories Present?</FormLabel>
-            <RadioGroup
-              row
-              aria-label="dormitoriesPresent"
-              name="dormitoriesPresent"
-              value={formData.dormitoriesPresent || ""}
-              onChange={handleChange("dormitoriesPresent")}
-            >
-              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="No" control={<Radio />} label="No" />
-            </RadioGroup>
-          </FormControl>
+        <Grid item xs={12} sm={12}>
+          <Card
+            variant="outlined"
+            sx={{
+              borderRadius: "12px",
+              border: "1px solid #EAECF0",
+              background: "#FFF",
+              boxShadow:
+                "0px 1px 3px 0px rgba(16, 24, 40, 0.10), 0px 1px 2px 0px rgba(16, 24, 40, 0.06)",
+              px: 3,
+              py: 2,
+            }}
+          >
+            <Grid container>
+              <Grid item xs={12} sm={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">
+                    Are Dormitories Present?
+                  </FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="dormitoriesPresent"
+                    name="dormitoriesPresent"
+                    value={formData.dormitoriesPresent || ""}
+                    onChange={handleChange("dormitoriesPresent")}
+                  >
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              {formData.dormitoriesPresent === "Yes" && (
+                <Grid item xs={12} sm={2.5}>
+                  <FormControl component="fieldset">
+                    <FormLabel component="legend">
+                      Are Dormitories Offsite/Onsite?
+                    </FormLabel>
+                    <RadioGroup
+                      row
+                      aria-label="dormitoriesLocation"
+                      name="dormitoriesLocation"
+                      value={formData.dormitoriesLocation || ""}
+                      onChange={handleChange("dormitoriesLocation")}
+                    >
+                      <FormControlLabel
+                        sx={formControlLabelStyle}
+                        value="Offsite"
+                        control={<Radio />}
+                        label="Offsite"
+                      />
+                      <FormControlLabel
+                        sx={formControlLabelStyle}
+                        value="Onsite"
+                        control={<Radio />}
+                        label="Onsite"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+              )}
+              <Grid item xs={12} sm={2.5}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">
+                    Are Kitchen / Canteen Present?
+                  </FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="kitchenPresent"
+                    name="kitchenPresent"
+                    value={formData.kitchenPresent || ""}
+                    onChange={handleChange("kitchenPresent")}
+                  >
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={2.5}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">
+                    Are there Multiple Tenants?
+                  </FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="multipleTenants"
+                    name="multipleTenants"
+                    value={formData.multipleTenants || ""}
+                    onChange={handleChange("multipleTenants")}
+                  >
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">
+                    Facility owns building(s)?
+                  </FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="ownsBuildings"
+                    name="ownsBuildings"
+                    value={formData.ownsBuildings || ""}
+                    onChange={handleChange("ownsBuildings")}
+                  >
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      sx={formControlLabelStyle}
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Card>
         </Grid>
-        {formData.dormitoriesPresent === "Yes" && (
-          <Grid item xs={12} sm={3}>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">
-                Are Dormitories Offsite/Onsite?
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-label="dormitoriesLocation"
-                name="dormitoriesLocation"
-                value={formData.dormitoriesLocation || ""}
-                onChange={handleChange("dormitoriesLocation")}
-              >
-                <FormControlLabel
-                  value="Offsite"
-                  control={<Radio />}
-                  label="Offsite"
-                />
-                <FormControlLabel
-                  value="Onsite"
-                  control={<Radio />}
-                  label="Onsite"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Grid>
-        )}
-        <Grid item xs={12} sm={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">
-              Are Kitchen / Canteen Present?
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-label="kitchenPresent"
-              name="kitchenPresent"
-              value={formData.kitchenPresent || ""}
-              onChange={handleChange("kitchenPresent")}
-            >
-              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="No" control={<Radio />} label="No" />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">
-              Are there Multiple Tenants?
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-label="multipleTenants"
-              name="multipleTenants"
-              value={formData.multipleTenants || ""}
-              onChange={handleChange("multipleTenants")}
-            >
-              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="No" control={<Radio />} label="No" />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Facility owns building(s)?</FormLabel>
-            <RadioGroup
-              row
-              aria-label="ownsBuildings"
-              name="ownsBuildings"
-              value={formData.ownsBuildings || ""}
-              onChange={handleChange("ownsBuildings")}
-            >
-              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="No" control={<Radio />} label="No" />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
+
         <Grid item xs={12} sm={3}>
           <TextFieldQuestionnaire>
             <TextField
